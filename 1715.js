7b0268be3069f4177c5af1a0c39bd4e622d432d4
JMK1007 <coxld010@naver.com>
2023-10-27 15:31:11 +0900

    priority queue

---
+++ 1715.js
@@ -0,0 +1,116 @@
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue() {
+    if (this.values.length === 0) return null;
+
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+//입력값 1개(1줄)
+// const input = require('fs').readFileSync(filePath).toString().trim();
+
+//입력값 여러개 (1줄)
+// let input = require('fs').readFileSync(filePath).toString().trim().split(' ');
+
+//입력값 여러 줄
+// let input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+// 입력값이 첫 번째 줄에는 입력 값의 길이(n), n개의 줄에 걸쳐서 한 줄에 하나의 입력값이 주어질 때
+const [n, ...input] = require('fs')
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n');
+
+function solution(N, Arr) {
+  const pq = new PriorityQueue();
+  let result = 0;
+
+  Arr.forEach((c) => {
+    pq.enqueue(c, c);
+  });
+
+  if (N < 2) {
+    return 0;
+  }
+
+  while (pq.values.length > 1) {
+    const v1 = pq.dequeue();
+    const v2 = pq.dequeue();
+
+    const sum = v1.val + v2.val;
+    result += sum;
+
+    pq.enqueue(sum, sum);
+  }
+
+  return result;
+}
+
+const answer = solution(Number(n), input.map(Number));
+
+console.log(answer);
